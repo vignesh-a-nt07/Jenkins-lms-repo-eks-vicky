c7820c3c39b1de827a7fffc5262de301414dde0d
vignesh-a-nt07 <vignesh.a@navattech.com>
2026-02-05 10:50:49 +0530

    terraform moved into new repo

--- terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "lms-vicky-terraform-state-492000"
-    key            = "lms/dev/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-state-lock"
-    encrypt        = true
-  }
-}
